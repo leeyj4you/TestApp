fe4a810790a77f67b0c2969f0a57d08b894060e2
jhlee21a <jhlee21a@gmail.com>
2022-04-18 14:58:41 +0900

    jh commit

--- app/src/main/java/com/example/testapp2/MainActivity.java
+++ app/src/main/java/com/example/testapp2/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView MyTextView = new TextView(this);
-        MyTextView.setText("Hello World!");
+        MyTextView.setText("YJ, Hello World!");
     }
 }
