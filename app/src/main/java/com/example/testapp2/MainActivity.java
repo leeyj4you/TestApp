74098a258143716d72fab161ded61b9a5f686faf
Yujin Lee <leeyj4you@gmail.com>
2022-04-18 14:54:22 +0900

    hello world

--- app/src/main/java/com/example/testapp2/MainActivity.java
+++ app/src/main/java/com/example/testapp2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.testapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView MyTextView = new TextView(this);
+        MyTextView.setText("Hello World!");
     }
 }
